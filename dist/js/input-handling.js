a866d5d09093064083307e0115d4e975800accbe
LACHLAN ALEXANDER HAROLD GORST <18948573@students.latrobe.edu.au>
2019-02-28 01:30:32 +1100

    HTML button spin on hover completed

---
+++ dist/js/input-handling.js
@@ -0,0 +1,7 @@
+const job1_HTML = document.querySelector('.job-1');
+const job2_CSS = document.querySelector('.job-2');
+const job3_JS = document.querySelector('.job-3');
+
+job1_HTML.addEventListener('click', htmlClick);
+job2_CSS.addEventListener('click', cssClick);
+job3_JS.addEventListener('click', jsClick);
