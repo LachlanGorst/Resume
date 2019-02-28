d63048c280060354b9de4d2592d9ae40525c231c
LACHLAN ALEXANDER HAROLD GORST <18948573@students.latrobe.edu.au>
2019-02-28 21:05:14 +1100

    5 language buttons done for large screens

--- dist/js/input-handling.js
+++ dist/js/input-handling.js
@@ -1,44 +1,82 @@
-const job1_HTML = document.querySelector('.job-1');
-const job2_CSS = document.querySelector('.job-2');
-const job3_JS = document.querySelector('.job-3');
-const job4_SASS = document.querySelector('.job-4');
-const job5_JQuery = document.querySelector('.job-5');
-
-const spot = document.querySelector('.spot');
-const spotStyle = getComputedStyle(spot);
-
-const userNameInput = document.querySelector('.inputName');
-const userInputBox = document.getElementById('userName');
-const jqueryH1 = document.querySelector('#jqueryH1');
-
-job1_HTML.addEventListener('click', htmlClick);
-job2_CSS.addEventListener('click', cssClick);
-job3_JS.addEventListener('click', jsClick);
-job4_SASS.addEventListener('click', sassClick);
-job5_JQuery.addEventListener('click', jqueryClick);
-
-userInputBox.addEventListener('keyup', e => {
-  if (e.keyCode === 13) {
-    jqueryH1.innerHTML = userInputBox.value;
-    jqueryH1.style.visibility = 'visible';
-    userNameInput.style.visibility = 'hidden';
+// * CSS Button Section //
+$('.job-2').click(() => {
+  if (!$('.css').hasClass('glow')) {
+    $('.css').addClass('glow');
+    $('.job-2').addClass('hidden');
+    $('.blurBack').removeClass('hidden');
+  } else {
+    $('.css').removeClass('glow');
+    $('.job-2').removeClass('hidden');
+    $('.blurBack').addClass('hidden');
   }
 });
+//Hover Version
+$('.job-2').mouseover(() => {
+  $('.css').addClass('glow');
+  $('.job-2').addClass('hidden');
+  $('.blurBack').removeClass('hidden');
+});
+$('.job-2').mouseleave(() => {
+  $('.css').removeClass('glow');
+  $('.job-2').removeClass('hidden');
+  $('.blurBack').addClass('hidden');
+});
+// * CSS Button Section End
 
-function htmlClick() {}
-function cssClick() {}
-
+// * JS Button Section //
+const job3_JS = document.querySelector('.job-3');
+job3_JS.addEventListener('click', jsClick);
 function jsClick(e) {
-  spot.style.left = `${e.clientX - 10}px`;
-  spot.style.top = `${e.clientY - 10}px`;
-  spot.style.visibility = 'visible';
+  $('.spot').css('left', `${e.clientX - 10}px`);
+  $('.spot').css('top', `${e.clientY - 10}px`);
+  $('.spot').addClass('spotClicked');
+  setTimeout(removeSpot, 500);
 }
+function removeSpot() {
+  $('.spot').removeClass('spotClicked');
+}
+// * JS Button Section End//
 
-function sassClick() {
-  let newBackground = '#fff';
-  job4_SASS.style.color = `set-text-color(${newBackground})`;
-}
+// * SASS Button Section //
+//Click Version For Mobile
+$('.job-4').click(() => {
+  if (!$('.job-4').hasClass('blue')) {
+    $('.job-4').addClass('blue');
+  } else {
+    $('.job-4').removeClass('blue');
+  }
+});
+//Hover Version
+$('.job-4').mouseover(() => {
+  $('.job-4').addClass('blue');
+});
+$('.job-4').mouseleave(() => {
+  $('.job-4').removeClass('blue');
+});
+// * SASS Button Section End//
+
+// * JQuery Button Section //
+/*
+const userNameInput = document.querySelector('.inputName'); //div
+const userInputBox = document.getElementById('userName'); //input
+const jqueryH1 = document.querySelector('#jqueryH1'); //h1
+const job5_JQuery = document.querySelector('.job-5'); //div box
+
+job5_JQuery.addEventListener('click', jqueryClick);
 function jqueryClick() {
   jqueryH1.style.visibility = 'hidden';
   userNameInput.style.visibility = 'visible';
-}
+} */
+$('.job-5').click(() => {
+  $('#jqueryH1').css('visibility', 'hidden');
+  $('.inputName').css('visibility', 'visible');
+});
+
+$('#userName').keyup(e => {
+  if (e.keyCode === 13) {
+    $('#jqueryH1').html($('#userName').val());
+    $('#jqueryH1').css('visibility', 'visible');
+    $('.inputName').css('visibility', 'hidden');
+  }
+});
+// * JQuery Button Section End //
